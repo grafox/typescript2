af26b4673f81c2167c10c57f78b8107d53040e55
Gassan <g_fox_fts@yahoo.com>
2020-06-21 18:41:42 +0200

    object type

---
+++ app2.ts
@@ -0,0 +1,17 @@
+function add(n1: number,n2: number):number{
+    //if (typeof n1!=='number' || typeof n2!=='number'){
+    //    throw new Error('Incorrect input!');  
+    //}
+    console.log(typeof(n1));
+    return n1+n2;
+}
+
+let number1:number;
+number1 = 5;
+let number2:number
+number2 = 2.8;
+
+// this solution convert string to number //const result = add(+number1,+number2);
+const result = add(number1,number2);
+
+console.log(result);
