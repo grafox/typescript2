aacf173022b4a5e6ce3f77c97943f510355ff6b4
Gassan <g_fox_fts@yahoo.com>
2020-06-21 18:11:23 +0200

    Add function

---
+++ app.js
@@ -0,0 +1,12 @@
+function add(n1, n2) {
+    //if (typeof n1!=='number' || typeof n2!=='number'){
+    //    throw new Error('Incorrect input!');  
+    //}
+    console.log(typeof (n1));
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+// this solution convert string to number //const result = add(+number1,+number2);
+var result = add(number1, number2);
+console.log(result);
