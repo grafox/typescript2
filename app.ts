1ff88beb7feef23684aab69d66b2e751d82c3c39
Gassan <g_fox_fts@yahoo.com>
2020-06-22 21:45:18 +0200

    object type

--- app.ts
+++ app.ts
@@ -2,10 +2,15 @@
 const person:{
     name: string;
     age: number;
+    hobbies: string[]
 } = {
     name: 'Gassan',
-    age: 30
+    age: 30,
+    hobbies:['Sport','Cooking']
 }
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sport'];
 
 console.log(person.name);
 
