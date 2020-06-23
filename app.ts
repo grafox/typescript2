266f44236abc6efc7ae4b79a1fdd092d56566cb3
Gassan <g_fox_fts@yahoo.com>
2020-06-23 07:33:26 +0200

    enum and Tulpes

--- app.ts
+++ app.ts
@@ -1,14 +1,31 @@
+//const ADMIN=0 // use enum better type
+//const READ_ONLY=1
+//const AUTHOR=2
+enum Role {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR
+};
 // object type
 const person:{
     name: string;
     age: number;
-    hobbies: string[]
+    hobbies: string[],
+    //role:[number,string] //tuples
+    role: Role
 } = {
     name: 'Gassan',
     age: 30,
-    hobbies:['Sport','Cooking']
+    hobbies:['Sport','Cooking'],
+    //role:[2,'author']
+    role:Role.ADMIN
 }
 
+//person.role.push('admin'); // push not working propaply wuth tulpe
+//person.role.push[1]=10; // asign (=) better for debug
+
+//person.role=[0,'admin']; //tuples
+person.role=Role.AUTHOR;
 let favoriteActivities: string[];
 favoriteActivities = ['Sport'];
 
