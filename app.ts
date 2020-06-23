d03c9947746a40979f6e840d16df8089c5f581b4
Gassan <g_fox_fts@yahoo.com>
2020-06-23 13:58:51 +0200

    combine

--- app.ts
+++ app.ts
@@ -1,33 +1,10 @@
-//const ADMIN=0 // use enum better type
-//const READ_ONLY=1
-//const AUTHOR=2
-enum Role {
-    ADMIN,
-    READ_ONLY,
-    AUTHOR
-};
-// object type
-const person:{
-    name: string;
-    age: number;
-    hobbies: string[],
-    //role:[number,string] //tuples
-    role: Role
-} = {
-    name: 'Gassan',
-    age: 30,
-    hobbies:['Sport','Cooking'],
-    //role:[2,'author']
-    role:Role.ADMIN
-}
+function combine(input1: number|string,input2: number|string):number{
+    let result;
+    if(typeof input1 === 'number' && typeof input2 === 'number'){
+        const result = input1+input2
+    }else{
+        result= input1.toString() + input2.toString();
+    }
+    return result}
 
-//person.role.push('admin'); // push not working propaply wuth tulpe
-//person.role.push[1]=10; // asign (=) better for debug
-
-//person.role=[0,'admin']; //tuples
-person.role=Role.AUTHOR;
-let favoriteActivities: string[];
-favoriteActivities = ['Sport'];
-
-console.log(person.name);
-
+const combinedAge = combine(30,26);
