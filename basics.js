9968b741ab827bcf9b733dd5e3e7a179e43b51ce
Gassan <g_fox_fts@yahoo.com>
2020-06-27 20:34:52 +0200

    tsconfig.json

---
+++ basics.js
@@ -0,0 +1,19 @@
+function add(n1, n2) {
+    //if (typeof n1!=='number' || typeof n2!=='number'){
+    //    throw new Error('Incorrect input!');  
+    //}
+    console.log(typeof (n1));
+    return n1 + n2;
+}
+var combineValue;
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+var number1;
+number1 = 5;
+var number2;
+number2 = 2.8;
+// this solution convert string to number //const result = add(+number1,+number2);
+var result = add(number1, number2);
+console.log(result);
