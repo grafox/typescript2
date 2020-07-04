5914d06aa82af0ddaaac832a98334fee4c04f5c0
Gassan <g_fox_fts@yahoo.com>
2020-07-04 23:04:40 +0200

    class and super

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,15 @@
 "use strict";
 class Department {
-    constructor(n) {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        //private id:string
+        //private name:string
         this.employees = [];
-        this.name = n;
+        //this.name= n
     }
     describe() {
-        console.log(`Department: ${this.name}`);
+        console.log(`Department: ${this.id} ${this.name}`);
     }
     addEployee(employee) {
         this.employees.push(employee);
@@ -15,9 +19,21 @@ class Department {
         console.log(this.employees);
     }
 }
-const accounting = new Department('Accounting');
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, 'IT'); // if you used constructor in extended class you have to use super
+        this.admins = admins;
+    }
+}
+const accounting = new Department('D1', 'Accounting');
+const it = new ITDepartment('d1', ['Gassan']);
 accounting.describe();
 /* const accountingCopy = {name:'dummy',describe: accounting.describe}
 accountingCopy.describe();
  */
+accounting.addEployee('Gassan');
+accounting.addEployee('Maha');
+//accounting.employees[1]='Dana'
+accounting.describe();
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 class Department{
-    name:string
-    employees:string[]=[]
-    constructor(n:string){
-        this.name= n
+    //private id:string
+    //private name:string
+    private employees:string[]=[]
+    constructor(private readonly id:string,public name:string){
+        //this.name= n
     }
     describe(){
-        console.log(`Department: ${this.name}`)
+        console.log(`Department: ${this.id} ${this.name}`)
     }
     addEployee(employee:string){
         this.employees.push(employee)
@@ -16,11 +17,21 @@ class Department{
     }
 }
 
-const accounting = new Department('Accounting');
+class ITDepartment extends Department{
+    constructor(id:string, public admins:string[]){
+        super(id,'IT');  // if you used constructor in extended class you have to use super
+        
+    }
+}
+const accounting = new Department('D1','Accounting');
+const it = new ITDepartment('d1',['Gassan'])
 accounting.describe()
 
 /* const accountingCopy = {name:'dummy',describe: accounting.describe}
 accountingCopy.describe();
  */
-
- 
+accounting.addEployee('Gassan')
+accounting.addEployee('Maha')
+//accounting.employees[1]='Dana'
+accounting.describe()
+accounting.printEmployeeInformation()
