e2e67f51c93fcc80cb3a75e98d0470a7b6ab66ac
Gassan <g_fox_fts@yahoo.com>
2020-07-05 22:12:00 +0200

    interface

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,21 @@
 "use strict";
+let user1;
+user1 = {
+    name: 'Gassan',
+    age: 47,
+    greet(phrase) {
+        console.log(phrase + '' + this.name);
+    }
+};
+let add;
+class thePerson {
+    constructor(n) {
+        if (n) {
+            this.name = n;
+        }
+    }
+    greet(phrase) {
+        console.log(`${phrase}${name}`);
+    }
+}
 //# sourceMappingURL=app.js.map
