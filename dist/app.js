ba0d53be203daeedbb4b7dd5412b2d8d24fcca29
Gassan <g_fox_fts@yahoo.com>
2020-07-05 23:09:23 +0200

    Type Guard

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,44 @@ const e1 = {
     privileges: ['Create-server'],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log(`Name: ${emp.name}`);
+    if ('privileges' in emp) {
+        console.log(`Start Date: ${emp.privileges}`);
+    }
+    if ('startDate' in emp) {
+        console.log(`Start Date: ${emp.startDate}`);
+    }
+}
+printEmployeeInformation(e1);
+//or
+printEmployeeInformation({ name: 'Gassan', startDate: new Date });
+class Car {
+    drive() {
+        console.log('driveing ...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('driving ...');
+    }
+    loadCargo(amount) {
+        console.log(`loading cargo ... ${amount}`);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    //   if ('loadCargo' in vehicle){  // or
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo('1000');
+    }
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -27,3 +27,54 @@ type Combinable = string | number
 type Numeric = number | boolean
 
 type Universal = Combinable&Numeric
+
+function add(a:number,b:number){
+    if (typeof a === 'string' || typeof b === 'string'){
+        return a.toString() + b.toString()
+    }
+    return a+b
+}
+type UnKnownEmployee = Employee | Admin
+
+function printEmployeeInformation(emp:UnKnownEmployee){
+    console.log(`Name: ${emp.name}`);
+    if('privileges' in emp){
+        console.log(`Start Date: ${emp.privileges}`);
+    }
+    if('startDate' in emp){
+        console.log(`Start Date: ${emp.startDate}`);
+    }
+}
+printEmployeeInformation(e1)
+//or
+printEmployeeInformation({name:'Gassan',startDate: new Date})
+
+class Car{
+    drive(){
+        console.log('driveing ...');
+        
+    }
+    
+}
+class Truck{
+    drive(){
+        console.log('driving ...');
+        
+    }
+    loadCargo(amount:string){
+        console.log(`loading cargo ... ${amount}`);
+    }
+}
+
+type Vehicle =Car|Truck
+
+const v1 = new Car()
+const v2 = new Truck()
+
+function useVehicle(vehicle:Vehicle){
+    vehicle.drive()
+ //   if ('loadCargo' in vehicle){  // or
+    if (vehicle instanceof Truck){
+        vehicle.loadCargo('1000')
+    }
+}
