e571ca0f90b6c633976a6056031280ca6ceea143
Gassan <g_fox_fts@yahoo.com>
2020-07-09 13:44:52 +0200

    old

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,16 @@
 "use strict";
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        var _a;
+        (_a = this.hostElement) === null || _a === void 0 ? void 0 : _a.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+const projInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
