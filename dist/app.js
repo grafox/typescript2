3c8e6edd4fe57d0e2938c29e8907d960bac10be8
Gassan <g_fox_fts@yahoo.com>
2020-07-10 22:42:39 +0200

    interact with DOM elements

--- dist/app.js
+++ dist/app.js
@@ -9,11 +9,24 @@ class ProjectInput {
         this.hostElement = document.getElementById('app');
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInput = this.element.querySelector('#title'); // as HTMLInputElement
+        this.descriptionInput = this.element.querySelector('#description'); // as HTMLInputElement
+        this.peopleInput = this.element.querySelector('#peopleInput'); // as HTMLInputElement
         this.attach();
+        this.configure();
     }
     attach() {
         var _a;
         (_a = this.hostElement) === null || _a === void 0 ? void 0 : _a.insertAdjacentElement('afterbegin', this.element);
+    }
+    submitHandler(event) {
+        var _a;
+        event.preventDefault();
+        console.log((_a = this.titleInput) === null || _a === void 0 ? void 0 : _a.value);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 }
 const projInput = new ProjectInput();
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput{
     templateElement: HTMLTemplateElement | null
     hostElement: HTMLDivElement | null
     element: HTMLFormElement
+    titleInput: HTMLInputElement | null
+    descriptionInput: HTMLInputElement | null
+    peopleInput: HTMLInputElement | null
 
     constructor(){
        // this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!
@@ -14,11 +17,26 @@ class ProjectInput{
 
         const importedNode = document.importNode(this.templateElement.content,true)
         this.element = importedNode.firstElementChild as HTMLFormElement
+        this.element.id = 'user-input'
+
+        this.titleInput = this.element.querySelector('#title') // as HTMLInputElement
+        this.descriptionInput = this.element.querySelector('#description') // as HTMLInputElement
+        this.peopleInput = this.element.querySelector('#peopleInput') // as HTMLInputElement
+        
         this.attach()
+        this.configure()
     }
-     private attach(){
+    private attach(){
         this.hostElement?.insertAdjacentElement('afterbegin',this.element)
     } 
+    private submitHandler(event:Event){
+        event.preventDefault()
+        console.log(this.titleInput?.value);
+        
+    }
+    private configure(){
+        this.element.addEventListener('submit',this.submitHandler.bind(this))
+    }
 }
 
 const projInput = new ProjectInput()
