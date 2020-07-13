907e8ca0a8282b399c237b76afab3b8b00c8f0e1
Gassan <g_fox_fts@yahoo.com>
2020-07-13 16:16:34 +0200

    Component Base Class

--- dist/app.js
+++ dist/app.js
@@ -82,6 +82,22 @@ function autobind(_target, _methodName, descriptor) {
     };
     return adjDescriptor;
 }
+// Component Base Class
+class Component {
+    constructor(templateId, hostElementId, insertAtStart, newElementId) {
+        this.templateElement = document.getElementById(templateId);
+        this.hostElement = document.getElementById(hostElementId);
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        if (newElementId) {
+            this.element.id = newElementId;
+        }
+        this.attach(insertAtStart);
+    }
+    attach(insertAtBeginning) {
+        this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin' : 'beforeend', this.element);
+    }
+}
 // ProjectList Class
 class projectList {
     constructor(type) {
